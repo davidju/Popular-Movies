7801a641adae236e14b5d52ea1eb3081793df838
David Ju <davidju@berkeley.edu>
2018-02-15 08:40:08 -0800

    set main action and status bar to black

--- app/src/main/java/com/davidju/popularmoviesone/activities/MainActivity.java
+++ app/src/main/java/com/davidju/popularmoviesone/activities/MainActivity.java
@@ -1,9 +1,15 @@
 package com.davidju.popularmoviesone.activities;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.davidju.popularmoviesone.FetchMoviesTask;
 import com.davidju.popularmoviesone.R;
@@ -16,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.black)));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(ContextCompat.getColor(this, android.R.color.black));
+        }
     }
 
     @Override

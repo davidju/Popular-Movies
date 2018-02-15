d81f5ffed3b7b1f4f716d7244df99d7058e383e1
David Ju <davidju@berkeley.edu>
2018-02-14 16:09:40 -0800

    setup Movie model

--- app/src/main/java/com/davidju/popularmoviesone/models/Movie.java
+++ app/src/main/java/com/davidju/popularmoviesone/models/Movie.java
@@ -2,4 +2,30 @@ package com.davidju.popularmoviesone.models;
 
 
 public class Movie {
+
+    private String title;
+    private String posterPath;
+    private String synopsis;
+    private String rating;
+    private String releaseDate;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }

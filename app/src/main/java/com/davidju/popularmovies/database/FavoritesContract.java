22d12723ff723b9ec93bfbd1decfddb50462c3bb
David Ju <davidju@berkeley.edu>
2018-02-21 10:45:14 -0800

    create database contract and helper

---
+++ app/src/main/java/com/davidju/popularmovies/database/FavoritesContract.java
@@ -0,0 +1,17 @@
+package com.davidju.popularmovies.database;
+
+import android.provider.BaseColumns;
+
+public class FavoritesContract {
+
+    public static final class FavoritesEntry implements BaseColumns {
+        public static final String TABLE_NAME = "favorites";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_SYNOPSIS = "synopsis";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+    }
+
+}

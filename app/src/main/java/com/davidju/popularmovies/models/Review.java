8240970686c9fd25b0e739fbcd551929aaf3e154
David Ju <davidju@berkeley.edu>
2018-02-22 13:15:54 -0800

    convert Trailer and Review models to implement Parceable

--- app/src/main/java/com/davidju/popularmovies/models/Review.java
+++ app/src/main/java/com/davidju/popularmovies/models/Review.java
@@ -1,13 +1,43 @@
 package com.davidju.popularmovies.models;
 
-public class Review {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review implements Parcelable {
 
     private String author;
     private String content;
 
+    public static final Parcelable.Creator<Review> CREATOR = new Parcelable.Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel source) {
+            return new Review(source);
+        }
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[0];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(author);
+        dest.writeString(content);
+    }
+
     public Review(String author, String content) {
         this.author = author;
         this.content = content;
+    }
+
+    public Review (Parcel in) {
+        author = in.readString();
+        content = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public String getAuthor() {
